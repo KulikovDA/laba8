985fc3cb94cc3fff9606e0547ab0ac81b73bdfdf
Arion <Arion@DESKTOP-9BU80RS>
2023-12-16 22:26:45 +0200

    Добавьте файлы проекта.

---
+++ laba8/Program.cs
@@ -0,0 +1,15 @@
+﻿//базовый 9
+// 1
+Console.WriteLine("Текущая дата: " + DateTime.Now.ToShortDateString());
+// 2
+Console.WriteLine("Введите время начала телепередачи в формате ЧЧ:ММ: ");
+string startTime = Console.ReadLine();
+Console.WriteLine("Введите продолжительность телепередачи в секундах: ");
+int dolgo = Convert.ToInt32(Console.ReadLine());
+TimeSpan start = TimeSpan.Parse(startTime);
+TimeSpan end = start.Add(new TimeSpan(0, 0, dolgo));
+Console.WriteLine("Время окончания телепередачи: " + end);
+int dolgoM = dolgo / 60;
+Console.WriteLine("Продолжительность телепередачи: " + dolgoM + " минут");
+int reklama = dolgoM / 5;
+Console.WriteLine("Количество рекламных пауз: " + reklama);
